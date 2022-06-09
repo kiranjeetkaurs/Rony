778fdd61df56d1129f7415412b0b2b2e1e467ce2
kiranjeetkaurs <kiranjeetkaurs@gmail.com>
2022-06-08 20:50:52 -0500

    Commented a java file

--- src/test/java/Practice/JavaAlphanumericRegex.java
+++ src/test/java/Practice/JavaAlphanumericRegex.java
@@ -1,26 +1,26 @@
-package Practice;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-//https://www.javaguides.net/2020/07/10-java-regular-expression-java-regex-examples.html
-public class JavaAlphanumericRegex {
-	
-	public static void main(String[] args) {
-		List < String > names = new ArrayList < String > ();
-
-		names.add("JavaGuides");
-		names.add("JavaGuides123");
-		names.add("JavaGuides-----////"); //Incorrect
-
-		String regex = "^[a-zA-Z0-9]+$";
-
-		Pattern pattern = Pattern.compile(regex);
-
-		for (String name: names) {
-			Matcher matcher = pattern.matcher(name);
-			System.out.println(matcher.matches());
-		}
-	}
-}
+//package Practice;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
+////https://www.javaguides.net/2020/07/10-java-regular-expression-java-regex-examples.html
+//public class JavaAlphanumericRegex {
+//	
+//	public static void main(String[] args) {
+//		List < String > names = new ArrayList < String > ();
+//
+//		names.add("JavaGuides");
+//		names.add("JavaGuides123");
+//		names.add("JavaGuides-----////"); //Incorrect
+//
+//		String regex = "^[a-zA-Z0-9]+$";
+//
+//		Pattern pattern = Pattern.compile(regex);
+//
+//		for (String name: names) {
+//			Matcher matcher = pattern.matcher(name);
+//			System.out.println(matcher.matches());
+//		}
+//	}
+//}
